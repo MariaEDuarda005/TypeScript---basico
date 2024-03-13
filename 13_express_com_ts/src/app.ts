449ac64e48ddd809d029f7198d7e6e36ac1f499d
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-03-13 08:40:44 -0300

    Começando o express

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -1,2 +1,17 @@
 // 1 - iniciando projeto
-console.log("Express + TS.");
+//console.log("Express + TS.");
+
+//2 - init express
+import express from 'express';
+
+const app = express();
+
+// usar para receber ou enviar algo
+app.get("/", (req, res) => {
+    return res.send("Hello Express!")
+})
+
+// escolhendo a porta quando ele for executado, e quando for executado exibir uma mensagem 
+app.listen(3000, () => {
+    console.log("Aplicação funcionado")
+})
