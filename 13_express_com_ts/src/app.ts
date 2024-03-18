877520df15b55ecf8c47d97ae8854b21c5efafd1
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-03-18 08:43:45 -0300

    atualizações 18/03

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -2,7 +2,7 @@
 //console.log("Express + TS.");
 
 //2 - init express
-import express, {Request,Response} from 'express';
+import express, {NextFunction,Request,Response} from 'express';
 
 const app = express();
 
@@ -68,6 +68,79 @@ app.get("/api/product/:id", (req: Request, res: Response) => {
         return res.json(product);
     }else{
         return res.send("produto não encontrado!")
+    }
+})
+
+//8 - rotas complexas
+app.get("/api/product/:id/review/:reviewId", (req: Request, res: Response) => {
+    console.log(req.params)
+
+    const productId = req.params.id
+    const reviewId = req.params.reviewId
+
+    return res.send(`Acessando a review ${reviewId} do produto ${productId}`)
+})
+
+//9 - rourter handler
+function getUser(req: Request, res: Response){
+    console.log(`Resgatando o usuario com o id: ${req.params.id}`)
+    return res.send("O usuario foi encontrado")
+}
+
+app.get("/api/user/:id", getUser
+
+)
+
+//10 - middleware - executar uma função entre a execução de uma rota, podemos utilizar para validações
+/*Se a atual função de middleware não terminar o ciclo de solicitação-resposta, 
+ela precisa chamar next() para passar o controle para a próxima função de middleware. 
+Caso contrário, a solicitação ficará suspensa.*/
+
+function checkUser(req: Request, res: Response, next: NextFunction){
+    if(req.params.id === "1"){
+        console.log("Pode seguir")
+        next() //para mostrar que pode seguir
+    }else{
+        console.log("Acesso restrito")
+    }
+}
+
+app.get("/api/user/:id/access", checkUser,(req: Request, res: Response) => {
+    return res.json({msg: "Bem vindo a area administrativa"})
+})
+
+//11 - middleware para todas as rotas (todo o middleware tem que ter a função next sendo executada para destravar arequisição do usuario)
+function showPath(req: Request, res: Response, next: NextFunction){
+    console.log(req.path)
+    next()
+}
+
+app.use(showPath)
+
+app.get("/", (req,res) => { 
+    return res.send("Hello Express")
+})
+
+//12 - req e res com generics
+app.get("/api/user/:id/details/:name", 
+    (
+        req: Request<{id: string; name:string}>, 
+        res: Response<{status: boolean}>
+    ) => {
+        console.log(`ID: ${req.params.id}`)
+        console.log(`Name: ${req.params.name}`)
+
+        return res.json({ status: true })
+})
+
+//13 - tratamento de erro, com blocos TRY/CATCH
+app.get("/api/error", (req: Request, res: Response) => {
+    try{
+        throw new Error("Algo deu errado")
+    }catch (e: any){
+        // any - pois não sabemos o que vai chegar
+        // res.statusCode = 500
+        res.status(500).json({msg: e.message})
     }
 })
 
