487ef71c074b295c0ff0c7ad9d8d236088eca536
MariaEDuarda005 <mariaesferreira05@gmail.com>
2024-04-17 11:28:26 -0300

    Atualizações 17/04

--- 15_JAVASCRIPT_MODERNO (ES6)/scripts.js
+++ 15_JAVASCRIPT_MODERNO (ES6)/scripts.js
@@ -131,3 +131,90 @@ products.map((product) => {
 
 
 console.log(products)
+
+// 5 - templates literals
+const userName = 'Maria'
+const age = 19
+
+console.log(`O nome do usuario é ${userName} e tem ${age} anos`)
+
+// 6 - Destructuring 
+const frutas = ["maça", "laranja", "mamão"]
+
+const [f1,f2,f3] = frutas
+
+console.log(f1)
+console.log(f2)
+console.log(f3)
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Perifericos",
+    color: "Cinza"
+}
+
+const {name: productName, price, category: productCategory, color} = productDetails
+
+console.log(`O nome do produto é ${productName}, custa R$${price}, pertence a categoria ${productCategory} e sua cor é ${color}`)
+
+// 7 - spread operator
+const a1 = [1,2,3]
+const a2 = [4,5,6]
+
+const a3 = [...a1, ...a2]
+
+console.log(a3)
+
+const a4 = [0,...a1,4]
+console.log(a4)
+
+const carName = {name: "Gol"}
+const carBrand = {brand: "VW"}
+const otherInfos = {km: 125000, price: 39499.99}
+
+const car = {...carName, ...carBrand, ...otherInfos, wheels: 4}
+console.log(car)
+
+// 8 - classe
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+
+const shirt = new Product("Camisa", 50)
+console.log(shirt)
+console.log(`Desconto na ${shirt.name}: R$${shirt.productWithDiscount(10)}`)
+console.log(`Desconto de: ${shirt.productWithDiscount(50)}`)
+
+const tenis = new Product("Tenis preto", 458)
+console.log(`Desconto no ${tenis.name}: R$${tenis.productWithDiscount(50)}`)
+
+// 9 - herança
+class ProductWithAttributes extends Product{
+    constructor(name, price, colors){
+        super(name,price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são:")
+        this.colors.forEach((color) => {
+            console.log(color)
+        });
+    }
+}
+
+const hat = new ProductWithAttributes("Chapeu", 29.99, ["Preto", "Azul", "Verde"])
+console.log(hat)
+console.log(`Desconto de: ${hat.productWithDiscount(35)}`)
+console.log(hat.showColors())
+// sem o undefined
+hat.showColors()
